f1db523503c875e76f4a8f55aa74168f4e73fe10
unknown <venkat@codinggeek.com>
2020-12-03 12:42:49 +0530

    Auth Facade Interface

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+
+import 'auth_failure.dart';
+import 'value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
