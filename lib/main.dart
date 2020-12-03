bd43d408ac2b80f245bbb91d747dd49b5a7b3477
unknown <venkat@codinggeek.com>
2020-12-03 15:52:39 +0530

    injectable

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:notes/injection.dart';
 
 Future<void> main() async {
+  configureInjection(Environment.prod);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
