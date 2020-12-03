9e291e3a89df8dee6b11680a8c4e0748205f24e6
unknown <venkat@codinggeek.com>
2020-12-03 16:44:19 +0530

    sign-in form UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:notes/injection.dart';
+import 'package:notes/presentation/core/app_widget.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
   await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
